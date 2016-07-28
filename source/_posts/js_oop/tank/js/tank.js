988abe81453b86597a7efd38cc335feb8bea0dfe
unofficial <cangku@unofficial.cn>
2016-07-28 17:59:11 +0800

    能骂人吗？需求不详细还让改改改，还要兼容低版本IE，诶呀我去！

--- source/_posts/js_oop/tank/js/tank.js
+++ source/_posts/js_oop/tank/js/tank.js
@@ -6,6 +6,9 @@
  */
 var Tank = (function() {
 
+    // 方向
+    var direction = ['up', 'right', 'down', 'left'];
+
     function Tank() {
         var args = [].slice.call(arguments);
         // tank的属性
@@ -16,12 +19,28 @@ var Tank = (function() {
         // hero
         if(this.roleName === 'hero') {
             // 英雄坦克固定位置，正向居中在地图上，等待命令
-            var x = (map.mapEle.clientWidth - this.tanker.clientWidth) / 2 + 'px';
-            var y = (map.mapEle.clientHeight - this.tanker.clientHeight) / 2 + 'px';
-            this.tanker.style.transform = 'translate3d('+ x + ',' + y + ', 0)';
+            var x = (map.mapEle.clientWidth - this.tanker.clientWidth) / 2;
+            var y = (map.mapEle.clientHeight - this.tanker.clientHeight) / 2;
+            this.tanker.style.transform = 'translate3d('+ x + 'px,' + y + 'px, 0)';
 
+            var self = this;
             // 控制移动
+            setTimeout(function() {
+                self.data = {
+                    direction: direction[0],
+                    src: {
+                        x: x,
+                        y: y
+                    },
+                    dest: {
+                        x: 10,
+                        y: 10
+                    }
+                };
 
+                self.move();
+
+            }, 3000);
         } else if(this.roleName === 'NPC') {
             // NPC坦克随机位置
 
@@ -39,28 +58,53 @@ var Tank = (function() {
     }
 
     // 上移动
-    function moveLeft() {
+    function moveUp() {
+        // 保持原来的X方向不变，Y方向正向移动
+        var x = this.data.src.x;
+        var y = this.data.dest.y;
+        this.tanker.style.transform = 'translate3d('+ x + 'px,' + y + 'px, 0)';
+        this.tanker.style.transition = 'transform 2.s';
+        this.data.src.y = y;
+    }
+    
+
+    // 右移动
+    function moveRight() {
 
     }
 
     // 下移动
-    function moveLeft() {
+    function moveDown() {
 
     }
 
     // 左移动
     function moveLeft() {
-
+        var x = this.data.dest.x;
+        var y = this.data.dest.y;
+        this.tanker.style.transform = 'translate3d('+ x + 'px,' + y + 'px, 0)';
+        this.tanker.style.transition = 'transform 2.s';
     }
 
-    // 右移动
-    function moveLeft() {
-
+    // 移动操作
+    // 从A移动到B
+    // tanker direction up
+    // eg: 50, 50 -> 10, 10
+    // up: 50 -> 10, left: 50 -> 10
+    // test
+    function move() {
+        this.moveUp();
+        this.moveLeft();
     }
 
 
     Tank.prototype = {
-        draw: draw
+        draw: draw,
+        move: move,
+        moveUp: moveUp,
+        moveRight: moveRight,
+        moveDown: moveDown,
+        moveLeft: moveLeft
     }
 
     return Tank;
