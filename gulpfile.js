1348c120e73870afee846dff79c118983c122272
cangku <cangku@unofficial.cn>
2016-08-05 17:58:10 +0800

    home update

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,53 @@
 let gulp = require('gulp');
 let sass = require('gulp-sass');
 let cleanCSS = require('gulp-clean-css');
-// tast -> default
-gulp.task('default', function() {
+
+
+/**
+ * param
+ * 1. css
+ * 2. js
+ * 
+ */
+
+// 资源目录
+let assets = './assets';
+
+// css目录
+let sassPath = assets + '/scss/*.scss';
+let cssSrc = assets + '/scss/style.scss';
+let cssDest = assets + '/css'
+
+// js目录
+let jsSrc = assets + '/js/es6/*.js';
+let jsDest = assets + '/js';
+
+
+// sass
+gulp.task('sass', function() {
 
 	// scss -> css
-	gulp.src('./assets/scss/reset.scss')
+	gulp.src(cssSrc)
 		.pipe(sass().on('error', sass.logError))
 		.pipe(cleanCSS())
-		.pipe(gulp.dest('./assets/css'));
+		.pipe(gulp.dest(cssDest));
 
 })
+
+// auto watch sass
+gulp.task('sass:watch', function() {
+
+	gulp.watch(sassPath, ['sass']);
+})
+
+// js
+gulp.task('js', function() {
+
+	// es6 -> es5
+	gulp.src(jsSrc)
+		.pipe()
+		.pipe(gulp.dest(jsDest));
+})
+
+// tast -> default
+gulp.task('default', ['sass', 'sass:watch'])
---
+++ readme.md
@@ -0,0 +1,19 @@
+### 静态资源处理
+
+### 安装工具依赖
+```
+
+// 全局 gulp 命令
+npm i -g gulp
+
+// 安装依赖
+npm i
+
+```
+
+### package.json 依赖说明
+* gulp: gulp自动化构建工具  
+* gulp-sass: sass->css插件工具
+* gulp-clean-css: css压缩工具
+
+  
