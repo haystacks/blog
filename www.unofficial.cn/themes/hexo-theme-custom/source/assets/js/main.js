0ff897a4a4bde9cd04a011f4ebb3fb035f4b44f3
cangku <cangku@unofficial.cn>
2016-08-15 17:43:05 +0800

    update comments

---
+++ www.unofficial.cn/themes/hexo-theme-custom/source/assets/js/main.js
@@ -0,0 +1,23 @@
+/* 评论数 */
+    $(function() {
+        // 固定参数
+        var short_name = 'unofficial';
+        var keys = [];
+        var $keys = $('.duoshuo_key');
+        $keys.each(function(k, v) {
+            keys.push(v.id);
+        })
+        
+        var url = "http://api.duoshuo.com/threads/counts.jsonp?short_name="+ short_name +"&threads=" + keys.join(',');
+        $.ajax({
+            dataType: "jsonp",
+            url: url,
+            success: function(data) {
+                var res = data.response;
+                console.log(res);
+                $keys.each(function(k, v) {
+                    $(v).html(res[v.id].comments);
+                })
+            }
+        });
+    });
