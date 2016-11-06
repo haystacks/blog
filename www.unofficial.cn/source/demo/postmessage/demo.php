f9ff999bc76494464e968ee8ae5d941574663c01
unofficial <cangku@unofficial.cn>
2016-11-06 21:10:29 +0800

    update

--- www.unofficial.cn/source/demo/postmessage/demo.php
+++ www.unofficial.cn/source/demo/postmessage/demo.php
@@ -1,7 +1,7 @@
 <?php
 	// header('Access-Control-Max-Age: ')
 	header('Access-Control-Allow-Origin: http://localhost:4000');
-	header('Content-Type: application/json');
+	// header('Content-Type: application/json');
 	header('Access-Control-Allow-Headers: Content-Type');
 	print_r($_REQUEST);
 ?>
