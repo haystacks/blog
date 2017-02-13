3135d72a0771cfc234166d225ff5d8ee4570fc0b
unofficial <cangku@unofficial.cn>
2017-02-13 17:31:33 +0800

    learn

---
+++ www.unofficial.cn/source/demos/page-admin/github.blur-admin/gulpfile.js
@@ -0,0 +1,44 @@
+'use strict';
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+
+
+/**
+ * param
+ * 1. css
+ * 2. js
+ * 
+ */
+
+// 资源目录
+let assets = './assets/';
+
+// css目录
+let sassPath = assets + 'scss/*.scss';
+let cssDest = assets + 'css'
+
+// js目录
+let jsSrc = assets + 'js/es6/*.js';
+let jsDest = assets + 'js';
+
+
+// sass
+gulp.task('sass', function() {
+
+	// scss -> css
+	gulp.src(sassPath)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(cleanCSS())
+		.pipe(gulp.dest(cssDest));
+
+})
+
+// auto watch sass
+gulp.task('sass:watch', function() {
+
+	gulp.watch(sassPath, ['sass']);
+})
+
+// tast -> default
+gulp.task('default', ['sass', 'sass:watch'])
