6f465bf64bbffd8c03a86a42333562205b92316d
unofficial <cangku@unofficial.cn>
2017-03-29 06:53:49 +0800

    update

--- www.unofficial.cn/source/demos/fanliapp/pages/home/index.js
+++ www.unofficial.cn/source/demos/fanliapp/pages/home/index.js
@@ -1,5 +1,32 @@
 Page({
+    data: {
+        msg: ''
+    },
+    onLoad() {
+        wx.getClipboardData({
+            success(res) {
+                console.log(res.data);
+            }
+        });
+    },
     getCoupon() {
-        console.log(123);
+        // 获取变化数据
+        let msg = this.msg;
+        wx.request({
+            url: 'http://fanli.unofficial.cn/api/app/index.php',
+            data: {
+                "msg": msg
+            },
+            header: {
+                "X-Requested-With": "XMLHttpRequest"
+            },
+            msthod: 'post',
+            success(res) {
+                console.log(res.data);
+            },
+            fail(res) {
+
+            }
+        })
     }
 })
