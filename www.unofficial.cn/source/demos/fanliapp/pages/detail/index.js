8447ac85ccdd8d8f301285227c8deb15e386f0a4
unofficial <cangku@unofficial.cn>
2017-03-31 07:09:50 +0800

    update

--- www.unofficial.cn/source/demos/fanliapp/pages/detail/index.js
+++ www.unofficial.cn/source/demos/fanliapp/pages/detail/index.js
@@ -11,17 +11,38 @@ Page({
         let self = this;
         wx.getStorage({
           key: k,
-          success: function(res){
+          success(res){
               let detail = JSON.parse(res.data);
                 self.setData({
                     detail: detail.status
                 })
           },
-          fail: function(res) {
+          fail(res) {
                 wx.redirectTo({
                       url: '../home/index'
                 })
           }
         })
+    },
+    toBuy() {
+        let tpwd = this.data.detail.tpwd;
+        if(wx.setClipboardData) {
+            wx.setClipboardData({
+                data: tpwd,
+                success() {
+                    wx.showToast({
+                        title: '成功',
+                        icon: 'success',
+                        duration: 2000
+                    })
+                } 
+            })
+        } else {
+            wx.showModal({
+                title: '提示',
+                content: '不支持自动复制淘口令，手动复制一下，立享优惠',
+                showCancel: false
+            })
+        }
     }
 })
