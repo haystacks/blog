2452346f7a43f0939ebc425ac47df2b0d8c783e1
unofficial <cangku@unofficial.cn>
2017-05-17 13:54:36 +0800

    update tool

--- www.unofficial.cn/source/tool/filetozip.php
+++ www.unofficial.cn/source/tool/filetozip.php
@@ -4,22 +4,58 @@
 	 * @ des zip压缩包补丁 更新站点程序
 	 * @ author unofficial
 	 * @ time 2017-05-03 10:56:32
+	 * @ update 2017-05-16 17:10:17
 	 */
 	
 	// config s
 	$dir	= './'; // username目录
 
 	// config e
+		/**
+	 * 获取当前目录及子目录下的所有文件
+	 * @param string $dir 路径名
+	 * @return array 所有文件的路径数组
+	 */
+	function get_files($dir) {
+		$files = array();
+	 
+		if(!is_dir($dir)) {
+			return $files;
+		}
+		$handle = opendir($dir);
+		if($handle) {
+			while(false !== ($file = readdir($handle))) {
+				if ($file != '.' && $file != '..') {
+					$filename = $dir . "/"  . $file;
+					if(is_file($filename)) {
+						$files[] = $filename;
+					}else {
+						$files = array_merge($files, get_files($filename));
+						// dir -- new
+						// $files[] = $filename.'/';
+					}
+				}
+			}   //  end while
+			closedir($handle);
+		}
+		// dir -- new
+		// $files[] = $dir.'/';
+		return $files;
+	}   //  end function
 	
 	function patch($value) {
 		$zip = new ZipArchive;
 		$filename = pathinfo($value, PATHINFO_FILENAME);
+		// mobile
 		$base = $filename.'/www/';
+		// pc
+		// $base = $filename.'/'.substr($filename, 2).'/';
+
+		$patchPath = './patch';
 		if ($zip->open($value) === TRUE && $zip->statName($base)) {
-			$rs = $zip->addFile('./patch/phpcms/libs/classes/attachment.class.php', $base.'phpcms/libs/classes/attachment.class.php');
-			$zip->addFile('./patch/phpcms/modules/content/down.php', $base.'phpcms/modules/content/down.php');
-			$zip->addFile('./patch/phpcms/modules/member/index.php', $base.'phpcms/modules/member/index.php');
-			$zip->addFile('./patch/phpcms/modules/pay/respond.php', $base.'phpcms/modules/pay/respond.php');
+			foreach (get_files('./patch') as $key => $value) {
+				$rs = $zip->addFile($value, $base.str_replace($patchPath.'/', '', $value));
+			}
 			$zip->close();
 			echo $filename." ok\n";
 		} else {
